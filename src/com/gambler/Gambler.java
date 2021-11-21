f31defc35c95ca0e19271b458b12059c7150a1f8
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-11-21 09:38:54 +0530

    Check Win or Loss

--- src/com/gambler/Gambler.java
+++ src/com/gambler/Gambler.java
@@ -1,11 +1,26 @@
 package com.gambler;
 
+import java.util.Random;
+
 public class Gambler {
-	public void game() {
-		System.out.println("Welcome Gambler");
-		int Every_Day_Stack=100;
-		int Bet_Every_Game=1;
+	static int Every_Day_Stack=100;  //initial amount we have
+	final static int Bet_Every_Game=1;    //every Bet amount
+	public void Game() {
+		System.out.println("Welcome To Gambler Simulator");
 		System.out.println("Every Day Stack: "+Every_Day_Stack);
 		System.out.println("Bet Price: "+Bet_Every_Game);
-	}
+			
+		Random random = new Random();  //taking random value between 0 and 1
+		int Bet = random.nextInt(2);
+		//checking win or loss condition
+		if(Bet == 1) {
+			System.out.println("Bet Win");
+			System.out.println("Final Amount: "+(Every_Day_Stack += Bet_Every_Game)); //increment amount by 1
+		}else {
+			System.out.println("Bet Loss");
+			System.out.println("Final Amount: "+(Every_Day_Stack -= Bet_Every_Game)); //decrement amount by 1
+		}
+			
+	}	
 }
+
--- src/com/gambler/TestGambler.java
+++ src/com/gambler/TestGambler.java
@@ -3,6 +3,6 @@ package com.gambler;
 public class TestGambler {
 	public static void main(String[] args) {
 		Gambler gambler = new Gambler();
-		gambler.game();
+		gambler.Game();
 	}
 }
